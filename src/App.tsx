43c1da83e352935cdaad20e467122cf8aac827e4
Max <santiago.farfan2001@gmail.com>
2023-08-25 16:51:15 -0400

    Base Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-
-return (
-  <div>
-    hey
-  </div>
-  );
-}
-
-
+  return <div className="m-0 p-0">hey</div>;
+};
 
 const container = document.getElementById("root");
 
